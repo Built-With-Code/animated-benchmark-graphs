96850d822940deb0f92f58a1b37b6f7aa889e63d
Akhil Waghmare <akhilwaghmare@gmail.com>
2023-11-25 15:28:12 -0800

    Starter code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,3 @@
-import BenchmarkCard from "@/components/BenchmarkCard";
-import { benchmarkData } from "@/db/BenchmarkData";
-
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col gap-20 py-24 px-48 bg-neutral-800">
-      <h1 className="text-white font-semibold text-6xl max-w-[24ch]">
-        M3 compared to previous MacBook Pro models.
-      </h1>
-      {benchmarkData.map((benchmark) => (
-        <BenchmarkCard {...benchmark} />
-      ))}
-    </main>
-  );
+  return <main>Animated benchmark graphs.</main>;
 }
